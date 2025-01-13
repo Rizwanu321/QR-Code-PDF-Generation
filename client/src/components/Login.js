9013d662324cdd5ab0b426a179e4f307e977d431
Rizwanu321 <142315620+Rizwanu321@users.noreply.github.com>
2025-01-13 22:18:03 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = ({ setIsAuthenticated, setUser }) => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://qr-code-pdf-generation-server.onrender.com/api/auth/login",
         credentials,
         { withCredentials: true }
       );
