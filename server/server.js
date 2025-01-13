fc10e8f62f54f422c71e49c51e193fdddc3d2472
Rizwanu321 <142315620+Rizwanu321@users.noreply.github.com>
2025-01-13 22:27:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://qr-code-pdf-generation-client.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
